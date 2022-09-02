1471605817cd9cd8508f6d59e524961d3aff0960
Spigushe <martin.cuchet@gmail.com>
2022-09-03 00:17:59 +0200

    Automatic reset of link styles

--- site_web/template/_footer.php
+++ site_web/template/_footer.php
@@ -3,23 +3,23 @@
         <p class="col-4">
             Vous pouvez retrouver le groupe de jeu nantais ici :
             <br>
-            <a href="https://discord.gg/pd9fgTe" class="text-reset" target="_blank"><i class="fa-brands fa-discord"></i></a>
-            <a href="https://discord.gg/pd9fgTe" class="text-reset" target="_blank">Discord</a>
+            <a href="https://discord.gg/pd9fgTe" target="_blank"><i class="fa-brands fa-discord"></i></a>
+            <a href="https://discord.gg/pd9fgTe" target="_blank">Discord</a>
             <br>
-            <a href="https://groups.google.com/g/vtes-nantes/about" class="text-reset" target="_blank"><i class="fa-solid fa-user-group"></i></a>
-            <a href="https://groups.google.com/g/vtes-nantes/about" class="text-reset" target="_blank">vtes-nantes</a>
+            <a href="https://groups.google.com/g/vtes-nantes/about" target="_blank"><i class="fa-solid fa-user-group"></i></a>
+            <a href="https://groups.google.com/g/vtes-nantes/about" target="_blank">vtes-nantes</a>
             <br>Code source du site :
-            <a href="https://github.com/Spigushe/vtes-website" class="text-reset" target="_blank" rel="noreferrer"><i class="fa-brands fa-github"></i></a>
-            <a href="https://github.com/Spigushe/vtes-website" class="text-reset" target="_blank" rel="noreferrer">GitHub</a>
+            <a href="https://github.com/Spigushe/vtes-website" target="_blank" rel="noreferrer"><i class="fa-brands fa-github"></i></a>
+            <a href="https://github.com/Spigushe/vtes-website" target="_blank" rel="noreferrer">GitHub</a>
 
         </p>
         <p class="col-4">
             Ce site est un contenu de fan non officiel.
             <br>Ni approuvé, ni promu par
-            <a href="https://www.blackchantry.com/" class="text-reset" target="_blank">Black Chantry Productions</a>
+            <a href="https://www.blackchantry.com/" target="_blank">Black Chantry Productions</a>
             <br>Certaines parties des matériaux utilisés sont la propriété de Black Chantry Productions.
-            <br>© <a href="https://www.blackchantry.com/" class="text-reset" target="_blank">Black Chantry Productions</a>.
-            <br>™ <a href="https://www.paradoxinteractive.com/" class="text-reset" target="_blank">Paradox Interactive AB.</a>
+            <br>© <a href="https://www.blackchantry.com/" target="_blank">Black Chantry Productions</a>.
+            <br>™ <a href="https://www.paradoxinteractive.com/" target="_blank">Paradox Interactive AB.</a>
         </p>
     </div>
 </footer>
@@ -42,6 +42,18 @@ function scrollFunction() {
         document.getElementById("vtes").classList.add("col-md-2");
     }
 }
+
+function resetLink(tagName) {
+    var element = document.getElementsByTagName(tagName);
+    for (let elt of element) {
+        var aTags = elt.getElementsByTagName("a");
+        for (let aTag of aTags) {
+            aTag.classList.add("text-reset");
+        }
+    }
+}
+resetLink("article");
+resetLink("footer");
 </script>
 </body>
 </html>
--- site_web/template/rise-of-the-fallen.php
+++ site_web/template/rise-of-the-fallen.php
@@ -1,4 +1,4 @@
-<article class="col-8 ms-auto me-auto pt-4">
+<article class="col-8 ms-auto me-auto py-4">
     <div class="col-12">
         <h2 class="text-center">Rise of the Fallen</h2>
         <p class="text-center mt-4">
@@ -15,17 +15,17 @@
     <div class="col-12">
         <div class="d-md-flex flex-row justify-content-around">
             <div class="col-md-3 text-center">
-                <a target="_blank" class="text-reset" href="https://calendar.google.com/event?action=TEMPLATE&tmeid=NDVlZnAxaGk0NWlqdTk3YzU2azV0bXQwNzIgYXNzby5uYW50ZXNqY2NAbQ&tmsrc=asso.nantesjcc%40gmail.com"><i class="fa-solid fa-calendar-plus"></i> Ajouter au calendrier</a>
+                <a target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&tmeid=NDVlZnAxaGk0NWlqdTk3YzU2azV0bXQwNzIgYXNzby5uYW50ZXNqY2NAbQ&tmsrc=asso.nantesjcc%40gmail.com"><i class="fa-solid fa-calendar-plus"></i> Ajouter au calendrier</a>
             </div>
             <div class="col-md-3 text-center">
-                <a target="_blank" class="text-reset" href="https://goo.gl/maps/tXFGzjoqe6JQTxRu6"><i class="fa-solid fa-map"></i> Voir sur Google Maps</a>
+                <a target="_blank" href="https://goo.gl/maps/tXFGzjoqe6JQTxRu6"><i class="fa-solid fa-map"></i> Voir sur Google Maps</a>
             </div>
             <div class="col-md-3 text-center">
-                <a target="_blank" class="text-reset" href="mailto:asso.nantesjcc+vtes@gmail.com?subject=Inscription%20-%20Tournoi%20Rise%20of%20the%20Fallen"><i class="fa-solid fa-envelope-open-text"></i> S'inscrire à l'événement</a>
+                <a target="_blank" href="mailto:asso.nantesjcc+vtes@gmail.com?subject=Inscription%20-%20Tournoi%20Rise%20of%20the%20Fallen"><i class="fa-solid fa-envelope-open-text"></i> S'inscrire à l'événement</a>
             </div>
 
             <div class="col-md-3 text-center">
-                <a target="_blank" class="text-reset" href="http://www.vekn.fr/forum/viewtopic.php?t=19867"><i class="fa-solid fa-comment-dots"></i> Discuter de l'événement</a>
+                <a target="_blank" href="http://www.vekn.fr/forum/viewtopic.php?t=19867"><i class="fa-solid fa-comment-dots"></i> Discuter de l'événement</a>
             </div>
 
         </div>
@@ -67,7 +67,7 @@
         </p>
         <p>
             ✉️ Pour s'inscrire au tournoi, merci d'envoyer un mail à
-            <a target="_blank" class="text-reset" href="mailto:asso.nantesjcc+vtes@gmail.com?subject=Inscription%20-%20Tournoi%20Rise%20of%20the%20Fallen">l'adresse suivante</a> avec les noms, prénoms et numéros VEKN des personnes que vous inscrivez.
+            <a target="_blank" href="mailto:asso.nantesjcc+vtes@gmail.com?subject=Inscription%20-%20Tournoi%20Rise%20of%20the%20Fallen">l'adresse suivante</a> avec les noms, prénoms et numéros VEKN des personnes que vous inscrivez.
             <br><em>Pour les personnes n'ayant pas de numéro VEKN, il faudra le préciser dans le mail pour la création d'un numéro.</em>
         </p>
         <div style="clear:both;"></div>
@@ -84,7 +84,7 @@
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d338.7702871615955!2d-1.554692415520547!3d47.21340660197572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4805eea5ff5a4b6b%3A0xa4884e99c78c4472!2sLes%20BerThoM!5e0!3m2!1sfr!2sfr!4v1661725218331!5m2!1sfr!2sfr" width="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
             <div class="col-md-3 text-center m-2">
-                <p><a target="_blank" class="light-red" href="mailto:asso.nantesjcc+vtes@gmail.com?subject=Inscription%20-%20Tournoi%20Rise%20of%20the%20Fallen">asso.nantesjcc@gmail.com</a></p>
+                <p><a target="_blank" href="mailto:asso.nantesjcc+vtes@gmail.com?subject=Inscription%20-%20Tournoi%20Rise%20of%20the%20Fallen">asso.nantesjcc@gmail.com</a></p>
             </div>
         </div>
     </div>
