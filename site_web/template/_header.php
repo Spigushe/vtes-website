c16e8883622733ba62203e25bf8a69809c2c72b9
Spigushe <martin.cuchet@gmail.com>
2022-09-04 11:49:32 +0200

    Nouvelle page

--- site_web/template/_header.php
+++ site_web/template/_header.php
@@ -11,6 +11,8 @@
     <title>VtES Nantes</title>
     <script src="https://kit.fontawesome.com/4ae9bdd55b.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet preload" href="https://static.krcg.org/web/krcg.css" as="style">
+    <script src="https://static.krcg.org/web/krcg.js"></script>
 </head>
 <body class="">
     <?php include("./template/_menu.php") ?>
---
+++ site_web/template/strategie-elementaire.php
@@ -0,0 +1,137 @@
+<article class="col-8 ms-auto me-auto my-4">
+    <div class="col-12">
+        <h2 class="text-center">Stratégie élémentaire</h2>
+        <p class="text-center mt-4">
+            Cette page est un court extrait du très bon article rédigé par Lionel Panhaleux sur le <a href="https://codex-of-the-damned.org/fr/strategy/index.html">Codex of the Damned</a>.
+            <br>Vous y trouverez un approfondissement sur les thèmes présentés ici.
+        </p>
+        <div class="d-md-flex flex-row flex-wrap justify-content-center">
+            <div class="col-md-3 text-center">
+                <a href="#strategie">
+                    <i class="fa-solid fa-chalkboard-user"></i>
+                    Les Fondamentaux
+                </a>
+            </div>
+            <div class="col-md-3 text-center">
+                <a href="#combat">
+                    <i class="fa-solid fa-people-arrows"></i>
+                    Le Déroulé d'un combat
+                </a>
+            </div>
+            <div class="col-md-3 text-center">
+                <a href="#regeneration">
+                    <i class="fa-solid fa-suitcase-medical"></i>
+                    La Régénération
+                </a>
+            </div>
+            <div class="col-md-3 text-center">
+                <a href="#archetypes">
+                    <i class="fa-brands fa-uncharted"></i>
+                    Les Principaux Archétypes
+                </a>
+            </div>
+        </div>
+        <div style="clear:both;"></div>
+    </div>
+</article>
+<?php separateur("strategie","dark"); ?>
+<article class="col-8 ms-auto me-auto my-4">
+    <div class="col-12">
+        <h2 class="text-center">Les Fondamentaux</h2>
+        <p class="text-center mt-4">
+            La base pour gagner consiste à détruire la réserve d'influence de sa proie et la manière la plus évidente de le faire est de <strong>mordre</strong>. Du côté défensif, les <strong>rebonds</strong> permettent de changer la cible d'une morsure.
+
+        </p>
+        <p class="text-center mt-4">
+            L'autre option pour réduire la réserve d'influence de la proie est d'utiliser des <strong>actions politiques</strong> comme <span class="krcg-card">Kine Resources Contested</span>. Les actions politiques font intervenir tous les vampires titrés en jeu pour déterminer si le vote passe. Les decks de vote agressifs vont passer plusieurs votes dans un seul tour, mais cela a conduit à l'utilisation de <span class="krcg-card">Delaying Tactics</span> comme défense.
+        </p>
+        <p class="text-center mt-4">
+            La charnière entre l'attaque et la défense est la gestion de la <strong>discrétion</strong> et de l'<strong>interception</strong>. De nombreuses disciplines donnent accès à de la discrétion et de l'interception.
+        </p>
+        <div style="clear:both;"></div>
+    </div>
+</article>
+<?php separateur("combat","dark"); ?>
+<article class="col-8 ms-auto me-auto my-4">
+    <div class="col-12">
+        <h2 class="text-center">Le Déroulé d'un combat</h2>
+        <p class="text-center mt-4">
+            Les combats surviennent lorsqu'un blocage réussit, mais peuvent aussi être provoqués directement par des actions comme <span class="krcg-card">Bum’s Rush</span> : à cause de cette carte, les autres actions avec un effet similaire sont communément appelées des actions de rush. Elle a aussi donné son nom à toute une catégorie de decks : les decks de rush.
+        </p>
+        <p class="text-center mt-4">
+            Du côté des disciplines, <span class="krcg-icon">p</span> et <span class="krcg-icon">h</span> infligent de grands quantités de dégâts. <span class="krcg-icon">i</span> a une combinaison puissante avec <span class="krcg-card">Aid from Bats</span> et <span class="krcg-card">Carrion Crows</span>, qui donnent 3 dégâts et une poursuite. <span class="krcg-icon">t</span> a l'excellent <span class="krcg-card">Theft of Vitae</span>, qui fournit gratuitement un différentiel de 4 sang sans possibilité de prévention.
+        </p>
+        <p class="text-center mt-4">
+            Il existe plusieurs moyens de se défendre durant les combats. La plus efficace est la <em>Frappe : Fin de combat</em>. La <strong>prévention</strong> de dégâts est une autre partie important du combat. <strong>Esquiver</strong> est aussi une bonne option défensive et le simple <span class="krcg-card">Dodge</span> est disponible pour tous.
+        </p>
+        <p class="text-center mt-4">
+            Avoir la bonne <strong>portée</strong> est essentiel au combat. La plupart des modules de combat se spécialisent pour une portée donnée, que ce soit à <strong>distance</strong> pour éviter le dégât de base que tout vampire peut infliger, ou au <strong>contact</strong> pour utiliser des frappes et des effets puissants.
+        </p>
+        <div style="clear:both;"></div>
+    </div>
+</article>
+<?php separateur("regeneration","dark"); ?>
+<article class="col-8 ms-auto me-auto my-4">
+    <div class="col-12">
+        <h2 class="text-center">La Régénération</h2>
+        <p class="text-center mt-4">
+            La réserve d'influence est la ressource clé du jeu. Plus on en a, mieux on se porte : plus d'influence signifie une meilleure survie et plus de serviteurs. Tous les decks efficaces ont un module de régénération élémentaire : un ensemble de cartes destinées à regagner de l'influence. Regagner du sang sur des vampires non contrôlés revient à régénérer l'influence, puisque le sang peut facilement être récupéré grâce à quelques transferts.
+        </p>
+        <div style="clear:both;"></div>
+    </div>
+</article>
+<?php separateur("archetypes","dark"); ?>
+<article class="col-8 ms-auto me-auto my-4">
+    <div class="col-12">
+        <h2 class="text-center">Les Principaux Archétypes</h2>
+
+        <h3 class="text-center mt-4">Les decks de morsure</h3>
+        <p class="text-center mt-4">
+            Les decks de morsure sont centrés sur l'agression de la proie et utilisent des actions de morsure pour infliger d'important dégâts à son influence. <span class="krcg-icon">d</span> est sans conteste la discipline donnant le plus de dégâts d'influence, et elle fournit de plus des cartes de rebond. <span class="krcg-icon">r</span> et <span class="krcg-icon">e</span> sont de bonnes alternatives.
+        </p>
+        <p class="text-center mt-4">
+            Les decks de morsure ont <strong>le plus haut niveau de dégâts d'influence</strong>. Ils ont tendance à éliminer leur première proie assez régulièrement, mais cela attire l'attention de la table sur eux et une élimination n'est pas suffisante pour la victoire. Ils sont en général <strong>faibles en combat</strong>. Ils ont tendance à se reposer sur des effets de « fin de combat », mais certaines variétés peuvent utiliser des dégâts aggravés ou des frappes envoyant en torpeur comme menace.
+        </p>
+
+        <h3 class="text-center mt-4">Les decks de vote</h3>
+        <p class="text-center mt-4">
+            Les decks de vote infligent en général de solides dégâts d'influence à leur proie. Le problème d'accumuler assez de voix pour gagner les référendums est contrebalancée par le bonus de +1 discrétion globalement sur les actions politiques. Il y a sommairement deux manières de construire un deck de vote : utiliser une technique de prolifération et trouver un moyen de donner des votes à tous les vampires, ou utiliser des vampires plus âgés ayant des titres en propre.
+        </p>
+        <p class="text-center mt-4">
+            Les decks de vote infligent d'<strong>importants dégâts d'influence</strong> une fois qu'ils obtiennent le verrou de vote. Ils ont aussi une <strong>bonne régénération</strong>, en utilisant <span class="krcg-card">Consanguineous Boon</span>, <span class="krcg-card">Parity Shift</span>, <span class="krcg-card">Ancient Influence</span> ou <span class="krcg-card">Political Stranglehold</span> selon les cas. <strong>La discrétion varie</strong> selon les cas, mais ils ne sont pas faciles à bloquer. Les constructions qui n'ont pas de hauts niveaux de discrétion se basent sur un essaim de serviteurs qui s'avèrera difficile à bloquer complètement. Enfin, les decks de cote on un <strong>module de combat léger</strong> et — en particulier les constructions à base d'essaim — une <strong>faible défense contre la morsure</strong>, qui repose uniquement sur les rebonds, ou même rien du tout.
+        </p>
+
+        <h3 class="text-center mt-4">Les decks mur</h3>
+        <p class="text-center mt-4">
+            Les decks mur maximisent leur défense en essayant de bloquer les actions de leur proie comme de leur prédateur tout en travaillant lentement à la régénération et aux dégâts d'influence. La capacité à intercepter et à se réveiller est indispensable dans ces decks, donc <span class="krcg-icon">a</span> et <span class="krcg-icon">i</span> sont les options les plus courantes. <span class="krcg-icon">a</span> fournit plus d'options et une plus haute interception au global, avec l'excellent <span class="krcg-card">Eyes of Argus</span>. C'est aussi la seule discipline à part <span class="krcg-icon">d</span> qui fournit des réactions de rebond. <span class="krcg-icon">i</span> a aussi de bonne cartes d'interception cependant, ainsi que de meilleurs options que <span class="krcg-icon">a</span> pour le combat : il peut être utilisé sans équipement ou discipline additionnels.
+        </p>
+        <p class="text-center mt-4">
+            Les decks mur ont sans surprise une <strong>défense exceptionnelle</strong>. De plus, leur grande capacité de blocage leur permet de jouer des cartes Maître fragiles qui demandent une protection pour être efficaces comme <span class="krcg-card">Smiling Jack, the Anarch</span>, <span class="krcg-card">The Rack</span>, <span class="krcg-card">Powerbase: Montreal</span>, <span class="krcg-card">Powerbase: Barranquilla</span> (Sabbat) et <span class="krcg-card">Anarch Revolt</span> (Anarchs), ce qui les aide pour la régénération et l'offensive. Toutefois, les decks mur ont une <strong>faible offensive</strong>, n'infligeant qu'une quantité de dégâts d'influence modérée à leur proie. Ils sont cependant très capables d'épuiser les autres decks autour de la table, et pourront finalement gagner une partie si aucun autre joueur n'obtient assez de Points de Victoire avant eux.
+        </p>
+
+        <h3 class="text-center mt-4">Les decks toolbox</h3>
+        <p class="text-center mt-4">
+            Les decks toolbox équilibrent leur capacités défensives et offensives. Ils ont une bonne interception, un module de combat décent et des options efficaces pour infliger des dégâts d'influence à leur proie, parfois même quelques votes qu'ils peuvent tenter de faire passer ou un peu de discrétion. Le clé de ces archétypes est d'utiliser des cartes versatiles fournissant de multiples options de jeu.
+        </p>
+        <p class="text-center mt-4">
+            La <strong>versatilité</strong> est évidemment le meilleur atout d'un deck toolbox. Il peut gérer et exploiter presque n'importe quelle configuration de table. Pour en tirer le meilleur parti, il faut faire preuve d'une grande sensibilité à l'équilibre de table et de bonnes capacités de négociation. Pour que le deck ait l'effet voulu à la table et fasse preuve de toute son efficacité, il est nécessaire de <strong>jouer sa main</strong>, quelle qu'elle soit. Le deck décide de la stratégie tout autant que le Mathusalem. Le joueur choisit toujours sur qui utiliser ses cartes, mais il doit trouver un moyen de les jouer pour que son deck excelle.
+        </p>
+
+        <h3 class="text-center mt-4">Les decks rush</h3>
+        <p class="text-center mt-4">
+            Les decks de rush utilisent le combat comme arme principale. Beaucoup de disciplines ont de bonnes options de combat et il y a aussi de bonnes cartes de combat ne nécessitant aucune discipline. Abattre les vampires à la table est amusant, mais n'aide pas nécessairement à faire baisser l'influence de la proie. Pour ce faire, une poignée de cartes peuvent aider, principalement <span class="krcg-card">Fame</span>, <span class="krcg-card">Dragonbound</span> et <span class="krcg-card">Tension in the Ranks</span>.
+        </p>
+        <p class="text-center mt-4">
+            La capacité à abattre n'importe quel serviteur donne à ces decks une <strong>solide défense</strong> contre leur prédateur. En fonction de l'archétype du prédateur, il est envisageable pour un deck de rush de cibler ses serviteurs en premier : cela peut nécessiter quelques combats pour les abattre, mais ils peuvent déactiver leur prédateur assez vite. D'un autre côté, ils ont une <strong>faible offensive</strong> et ont des difficultés à trouver un moyen d'infliger des dégâts d'influence suffisants à leur proie. C'est un problème clé pour ces archétypes.
+        </p>
+
+        <h3 class="text-center mt-4">Les decks spéciaux</h3>
+        <p class="text-center mt-4">
+            Certain decks n'entrent dans aucune des catégories susmentionnées. Ces construictions rares reposent sur des combinaisons de cartes exceptionnelles qui ont démontré leur étonnante efficacité. Les exemples qui suivent ne sont pas exhautifs.
+        </p>
+        <p class="text-center mt-4">
+            Les decks spéciaux ont tendance à utilisent des combinaisons de cartes complexes pour un effet dévastateur. Leur force réside dans l'<strong>avantage inconstestable</strong> accordé par leur combinaison si tout fonctionne comme prévu. Leur faiblesse est la fragilité d'une telle combinaison. Bloquer la bonne action ou contrer la bonne carte Maître peut souvent les arrêter net. De plus, les combinaisons connues ont tendance à attirer l'attention de tous les joueurs, ce qui conduit souvent le Mathusalem à perdre tous ses alliés très tôt dans la partie.
+        </p>
+        <div style="clear:both;"></div>
+    </div>
+</article>
