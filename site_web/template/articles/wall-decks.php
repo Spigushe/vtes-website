18a25eb0ad82585ae92e6924c1bde417c41266c7
Spigushe <martin.cuchet@gmail.com>
2022-09-11 22:53:28 +0200

    Wall Decks

---
+++ site_web/template/articles/wall-decks.php
@@ -0,0 +1,140 @@
+<article class="col-8 ms-auto me-auto py-4">
+    <div class="col-12">
+        <p class="text-end mt-4">
+            Mis en ligne initialement sur <a href="http://www.thelasombra.net/" target="_blank">The Lasombra</a>
+            <br>
+            Formattage et corrections mineures par <a href="https://gamingwithbs.wordpress.com/" target="_blank">Brett S.</a>
+            <br>
+            <a href="https://gamingwithbs.files.wordpress.com/2015/12/wall-decks.pdf" target="_blank">Article sauvé</a> visité le 11 septembre 2022
+            <br>
+            Par Derek RAY
+            <br>
+            Traduit par Martin CUCHET
+        </p>
+        <h2 class="text-center">Les Decks wall</h2>
+    </div>
+    <div style="clear:both;"></div>
+</article>
+<?php separateur(); ?>
+
+<article class="col-8 ms-auto me-auto p-4">
+    <div class="col-12">
+        <p class="">
+            <strong>
+                La seule manière pour un deck wall de perdre de le Game Win est de laisser sa proie gagner des Victory Points ou de laisser la table aller au temps.
+            </strong>
+            Relisez cette phrase, je sais qu'elle va en surprendre plus d'une personne.
+        </p>
+        <p>
+            Un deck wall a un contrôle total sur le Game Win. Les seules façons dont il peut échouer pour remporter la victoire sont :
+        </p>
+        <ul>
+            <li>
+                Permettre à sa proie d'obtenir 3 Victory Points, éliminant ainsi toute autre personne de la compétition pour le Game Win ;
+            </li>
+            <li>
+                Permettre à sa proie d'obtenir 2 Victory Points et à une autre personne d'obtenir 1 Victory Point, garantissant que le mieux que le deck wall puisse faire est d'égaliser ou de permettre à un autre joueur d'obtenir un Game Win ;
+            </li>
+            <li>
+                Permettre à sa proie d'obtenir 1 Victory Point et à une autre personne d'obtenir 2 Victory Points ; cela revient à la situation ci-dessus ;
+            </li>
+            <li>
+                Laissez la table aller au temps, garantissant que personne n'obtiendra un Game Win.
+            </li>
+        </ul>
+        <p>
+            Aucune de ces options n'est acceptable lorsque vous jouez avec un  deck wall. Par conséquent, il devrait y avoir deux objectifs principaux :
+        </p>
+        <ol type="1">
+            <li>
+                Empêcher sa proie de ne gagner aucun Victory Point par quelque moyen que ce soit ;
+            </li>
+            <li>
+                S'assurer que la table n'aille au temps en aucune circonstance.
+            </li>
+        </ol>
+    </div>
+</article>
+<?php separateur(); ?>
+
+<article class="d-md-flex flex-row justify-content-around">
+    <div class="col-md-8 p-3 px-4">
+        <p>
+            Le premier critère exige l'utilisation de <span class="krcg-card">Eagle's Sight</span>, qui exige Auspex supérieur <span class="krcg-icon">A</span>. Aucun autre moyen n'est suffisant. Même <span class="krcg-card">Eagle's Sight</span> n'est peut-être pas suffisant contre certains decks de sa proie, mais cela suffira pour la plupart des parties.
+        </p>
+        <p>
+            De plus, le premier critère exige également que le moins d'actions possibles de sa proie ne réussissent. Aucune action qui cause des dommages à votre grande proie, ou qui augmente la réserve d'influence de sa proie d'une manière ou d'une autre, ne devrait être autorisée à se résoudre si possible.
+        </p>
+        <p>
+            Le bon sens est appelé ici : si vous allez vous faire tuer en bloquant une morsure de 1, peut-être faut-elle la laisser passer. Mais la survie des sbires est secondaire par rapport au Game Win.
+        </p>
+        <p>
+            Puisque l'on ne peut pas gagner par inaction, il faut être aussi antimatérialiste que nécessaire pour empêcher sa proie de gagner des Victory Points. Parfois, cela signifie faire exploser l'un de ses propres serviteurs, mais cela peut être nécessaire.
+        </p>
+    </div>
+</article>
+<?php separateur(); ?>
+
+<article class="d-md-flex flex-row justify-content-around">
+    <div class="col-md-8 p-3 px-4">
+        <p>
+            Le deuxième critère ne peut être satisfait que par des cartes telles que la très vilipendée <span class="krcg-card">Anarch Revolt</span> et <span class="krcg-card">Smiling Jack, the Anarch</span>.
+        </p>
+        <p>
+            Cependant, cela ne veut pas dire qu'il faut dépenser les Anarchs comme si ce n'étaient pas des cartes importantes. Au lieu de cela, il vaut mieux garder un ou deux Anarchs en jeu en permanence.
+        </p>
+        <p>
+            Bloquer les actions visant à les détruire, à moins qu'elles ne soient entreprises par sa proie, ne semble pas nécessaire : elles ont déjà blessé la personne la plus importante, sa proie.
+        </p>
+        <p>
+            Combiné avec la lourde chaîne que vous attachez autour des chevilles de sa proie, elle devrait être presque complètement incapable de faire autre chose que d'être pitoyablement évincée par les Anarchs car le deck a des solutions de gain de réserve d'influence via des <span class="krcg-card">Blood Doll</span> et bloque les actions qui lui infligent de la perte de réserve d'influence. Les dégâts continus devraient être plus que suffisants pour lui survivre.
+        </p>
+    </div>
+</article>
+<?php separateur(); ?>
+
+<article class="d-md-flex flex-row justify-content-around">
+    <div class="col-md-8 p-3 px-4">
+        <p>
+            Une fois votre proie morte, arrêtons-nous un instant et revoyons nos critères. Tout d'abord, notre proie n'a pas eu de Victory Point, et nous en avons un. La table ne va certainement pas aller au temps, car les Anarchs et, peut-être, Smiling Jack sont un fardeau même pour les decks générant le plus de points de réserve d'influence. Que pourrait-il se passer sur le reste de la table ?
+        </p>
+        <p>
+            Peu de choses seulement :
+        </p>
+        <ol type="1">
+            <li>
+                Il est très probable que notre grande proie, n'ayant aucune pression pendant toute la partie, ait évincé notre grand-prédateur et notre prédateur, gagnant 2 Victory points. Cependant, elle doit encore nous évincer pour gagner un Game Win, et nous avons le dessus sur elle puisque notre deck, comme tous les decks wall, devient un monstre en un contre un. Nous aimons cette situation, en particulier avec les Anarchs sur la table qui lui forcent la main et l'empêche de récupérer des points de réserve d'influence et de récupérer un trop grand nombre de vampire. Le seul problème va être que nous devons obtenir l'Edge dès que possible pour réduire son gain en réserve d'influence, et notre deck n'a pas beaucoup d'options pour la discrétion. Quelques <span class="krcg-card">Night Moves</span> pourraient être un excellent choix.
+            </li>
+            <li>
+                Notre grand-proie est pathétique et notre grand prédateur a évincé notre prédateur, gagnant un Victory Point, <em>ou notre grande proie a évincé notre grand prédateur mais n'a pas encore fini le travail sur notre prédateur</em>. Alors que notre deck est fort dans un environnement à trois joueurs, les conditions ne sont pas encore idéales pour que nous puissions forcer la situation et gagner. Nous aimerions que ce soit en un contre un. De manière pratique, nous pouvons le faire en changeant de stratégie : ignorer complètement notre proie et tuer notre prédateur. Une fois notre prédateur évincé, le score sera de 1-1-1, et nous sommes dans la situation de « duel » que nous souhaitions au départ. Nous devrions pouvoir gagner facilement à partir d'ici.
+            </li>
+            <li>
+                Toute la table se débat impuissante sous le poids des Anarchs. Asseyez-vous, défendez, gagnez des points de réserve d'influence et regardez pour voir en laquelle des deux situations ci-dessus la partie évolue. Avec les Anarchs sortis, une personne ne devrait pas tarder à perdre la partie.
+            </li>
+        </ol>
+    </div>
+</article>
+<?php separateur(); ?>
+
+<article class="d-md-flex flex-row justify-content-around">
+    <div class="col-md-8 p-3 px-4">
+        <p>
+            Sommes-nous susceptibles d'être la cible de la table ? Les Anarchs ne nous rendront pas populaire, mais une fois qu'il sera clair que vous n'allez pas faire n'importe quoi avec eux ou empêcher qu'ils soient détruits, les gens se calmeront quelque peu.
+        </p>
+        <p>
+            Votre proie va être assez énervée à ce sujet et elle devrait l'être, c'est elle qui la prend dans le cul, mais votre grande proie va être votre ami.
+        </p>
+        <p>
+            Étant donné que votre grande proie n'a pas de pression, il est très probable qu'elle devienne elle-même la cible de la table : elle aura beaucoup plus d'actions la faisant avancer dans sa partie que vous, et cela attire l'attention, que les gens s'en rendent compte ou non.
+        </p>
+    </div>
+</article>
+<?php separateur(); ?>
+
+<article class="d-md-flex flex-row justify-content-around">
+    <div class="col-md-8 p-3 px-4">
+        <p>
+            Au moment où la partie devient un jeu à trois personnes, tout le monde est une cible de toute façon. Votre tâche est d'éviter d'être pris pour cible pendant les phases à 4 ou 5 joueurs ; à partir de ce moment-là, vous serez en bien meilleure forme.
+        </p>
+    </div>
+</article>
